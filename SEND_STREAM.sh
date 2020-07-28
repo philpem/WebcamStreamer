ba776e17c4c776cc37f4bd30003e90ee9cf12b36
Phil Pemberton <philpem@philpem.me.uk>
2020-07-28 12:36:50 +0100

    drop send rate to 15fps

--- SEND_STREAM.sh
+++ SEND_STREAM.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ffmpeg -i /dev/video0 -codec libx264 -f mpegts -tune zerolatency udp://224.0.0.1:9999
+ffmpeg -i /dev/video0 -codec libx264 -f mpegts -tune zerolatency -r 15 udp://224.0.0.1:9999
