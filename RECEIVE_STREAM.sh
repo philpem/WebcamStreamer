eb342155240039e65fdc64b0c84ab0fe94b5bece
Phil Pemberton <phil.pemberton@redembedded.com>
2020-07-29 10:17:58 +0100

    fix kmod path

--- RECEIVE_STREAM.sh
+++ RECEIVE_STREAM.sh
@@ -2,7 +2,7 @@
 
 # requires sudo to load the module
 
-sudo insmod ./v4l2loopback.ko devices=1 card_label="LoopbackCam" exclusive_caps=1
+sudo insmod ./v4l2loopback/v4l2loopback.ko devices=1 card_label="LoopbackCam" exclusive_caps=1
 
 ffmpeg -i udp://@224.0.0.1:9999 -analyzeduration 1 -fflags -nobuffer -probesize 32  -c:v rawvideo -pix_fmt yuyv422 -f v4l2 /dev/video0
 
