4d547317f2abec9a0c041773a007296de94d2d09
Phil Pemberton <phil.pemberton@redembedded.com>
2020-08-03 16:24:41 +0100

    Autodetect loopback device

--- RECEIVE_STREAM.sh
+++ RECEIVE_STREAM.sh
@@ -2,7 +2,18 @@
 
 # requires sudo to load the module
 
-sudo insmod ./v4l2loopback/v4l2loopback.ko devices=1 card_label="LoopbackCam" exclusive_caps=1
+CARDLABEL="LoopbackCam"
 
-ffmpeg -i udp://@224.0.0.1:9999 -analyzeduration 1 -fflags -nobuffer -probesize 32  -c:v rawvideo -pix_fmt yuyv422 -f v4l2 /dev/video0
+sudo insmod ./v4l2loopback/v4l2loopback.ko devices=1 card_label="$CARDLABEL" exclusive_caps=1
+
+for i in /sys/class/video4linux/video*; do
+	if [ "$(cat $i/name)" == "$CARDLABEL" ]; then
+		VIDEODEV=$(basename "$i")
+		break
+	fi
+done
+
+echo "Video device is /dev/$VIDEODEV"
+
+ffmpeg -i udp://@224.0.0.1:9999 -loglevel fatal -analyzeduration 1 -fflags -nobuffer -probesize 32  -c:v rawvideo -pix_fmt yuyv422 -f v4l2 /dev/$VIDEODEV
 
